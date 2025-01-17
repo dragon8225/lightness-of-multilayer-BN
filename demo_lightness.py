a614ffd1d02a3451892a4261c3bb971c393b6c75
dragon8225 <123951232+dragon8225@users.noreply.github.com>
2025-01-18 01:05:35 +0800

    Add files via upload

--- demo_lightness.py
+++ demo_lightness.py
@@ -6,10 +6,10 @@ if __name__ == '__main__':
     
     
     layer = 4
-    dimension = 2 * layer #变量个数
-    #弧度制角度#
+    dimension = 2 * layer 
+
     theta = [0., 0., 0., 0.]
-    #单位为lc的厚度#
+
     thick = [0.4, 0.4, 0.4, 0.4]
 
     light1 = light(dimension, theta, thick)
